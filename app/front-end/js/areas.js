6458d6bd3f0d6053380578c4d12d9e678f575474
R Boudewijn <Robert@robertboudewijn.nl>
2025-10-17 17:02:44 +0200

    Update card styles and area classification in front-end

--- app/front-end/js/areas.js
+++ app/front-end/js/areas.js
@@ -3,7 +3,7 @@ initAreas();
 
 function initAreas() {
     retrieveAreas();
-    setInterval(retrieveAreas, 60 * 1000); 
+    setInterval(retrieveAreas, 60 * 1000);
 }
 
 
@@ -12,7 +12,35 @@ async function retrieveAreas() {
     const response = await request.json();
     document.querySelector('.areas').innerHTML = '';
     response.data.forEach(area => {
-        document.querySelector('.areas').innerHTML += `<span class="huntable_card ${area.status}">${area.name}</span>`;
+        let classname = 'huntable_card ' + area.status;
+        switch (area.name) {
+            case "Alpha":
+                classname += " blue_border";
+                break;
+            case "Bravo":
+                classname += " red_border";
+                break;
+            case "Charlie":
+                classname += " yellow_border";
+                break;
+            case "Delta":
+                classname += " green_border";
+                break;
+            case "Echo":
+                classname += " purple_border";
+                break;
+            case "Foxtrot":
+                classname += " pink_border";
+                break;
+            case "Golf":
+                classname += " orange_border";
+                break;
+            case "Hotel":
+                classname += " grey_border";
+                break;
+        }
+
+        document.querySelector('.areas').innerHTML += `<span class="${classname}">${area.name}</span>`;
     });
 }
 
